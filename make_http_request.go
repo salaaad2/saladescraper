098da6f5b7f85512a08ddeb47ff3537c708380f5
salaaad2 <arthurdurant263@gmail.com>
2022-01-23 11:45:48 +0100

    amazing work, this is close

--- make_http_request.go
+++ make_http_request.go
@@ -1,24 +1,28 @@
 package main
 
 import (
-	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Welcome to this amazing web scraper. this is intended for substack")
+	fmt.Println("--- Welcome to this amazing web scraper. this is intended for substack ---")
 	name := os.Args[1]
 	fmt.Println(name)
 
+	// create url from parameter
 	one := "https://"
 	two := one + name
 	three := ".substack.com"
 	full := two + three
 
-	fmt.Println("Trying to get all articles from : ", full)
+	searchStr := full + "/p/"
+
+	fmt.Println("INFO: Trying to get all articles from : ", full)
 
 	response, err := http.Get(full)
 	if err != nil {
@@ -26,10 +30,39 @@ func main() {
 	}
 	defer response.Body.Close()
 
-	n, err := io.Copy(os.Stdout, response.Body)
+	// get all content and convert it into a string
+	dataInBytes, err := ioutil.ReadAll(response.Body)
+	pageContent := string(dataInBytes)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Println("bytes : ", n)
+	// look for posts
+	titleStart := strings.Index(pageContent, searchStr)
+	titleEnd := strings.Index(pageContent, "class=\"post-preview-description\"")
+	count := 0
+	for titleStart != -1 {
+		fmt.Println("DEBUG: start index: ", titleStart, "end index: ", titleEnd, "total length :", len(pageContent))
+
+		if titleEnd == -1 || titleStart == -1 {
+			break
+		}
+
+		pageTitle := (pageContent[titleStart + len(searchStr):titleEnd - 2])
+		fmt.Println("INFO: found the following post :\n ", pageTitle)
+		fmt.Println("DEBUG: start index: ", titleStart, "end index: ", titleEnd)
+
+		pageContent = pageContent[titleEnd + len("class=\"post-preview-title newsletter\""):]
+
+		titleStart = strings.Index(pageContent, searchStr)
+		titleEnd = strings.Index(pageContent, "class=\"post-preview-title newsletter\"")
+
+		count++
+	}
+	if count == 0 {
+		fmt.Println("ERROR: no posts to be found")
+	} else {
+		fmt.Println("INFO: found [", count, "] posts.")
+	}
 }
+// <a href="https://graymirror.substack.com/p/a-clarification-on-ukraine" class="post-preview-title newsletter">
